1ed266922a8b1e2f1eabd482ce3c07e00a959d89
vsilaev <vsilaev@gmail.com>
2026-02-09 11:13:02 +0300

    fix spring security contextualizer

--- net.tascalate.async.spring.webservlet/src/main/java/net/tascalate/async/spring/webservlet/RealSpringSecurityContextualizer.java
+++ net.tascalate.async.spring.webservlet/src/main/java/net/tascalate/async/spring/webservlet/RealSpringSecurityContextualizer.java
@@ -24,20 +24,9 @@
  */
 package net.tascalate.async.spring.webservlet;
 
-import java.lang.invoke.CallSite;
-import java.lang.invoke.LambdaConversionException;
-import java.lang.invoke.LambdaMetafactory;
-import java.lang.invoke.MethodHandle;
-import java.lang.invoke.MethodHandles;
-import java.lang.invoke.MethodType;
-import java.util.concurrent.Executor;
-import java.util.function.BiConsumer;
-
-import org.springframework.security.concurrent.DelegatingSecurityContextExecutor;
 import org.springframework.security.concurrent.DelegatingSecurityContextRunnable;
 import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.context.SecurityContextHolderStrategy;
 
 class RealSpringSecurityContextualizer extends SpringSecurityContextualizer {
     
@@ -47,54 +36,11 @@ class RealSpringSecurityContextualizer extends SpringSecurityContextualizer {
 
     @Override 
     Runnable contextualize(Runnable code) {
-        SecurityContextHolderStrategy strategy = SecurityContextHolder.getContextHolderStrategy();
-        if (null != strategy && null != SET_SECURITY_CONTEXT_HOLDER_STRATEGY) {
-            DelegatingSecurityContextExecutor executor = new DelegatingSecurityContextExecutor(RUN_IN_PLACE);
-            SET_SECURITY_CONTEXT_HOLDER_STRATEGY.accept(executor, strategy);
-            return () -> executor.execute(code);
+        SecurityContext ctx = SecurityContextHolder.getContext();
+        if (null != ctx) {
+            return DelegatingSecurityContextRunnable.create(code, ctx);
         } else {
-            SecurityContext ctx = SecurityContextHolder.getContext();
-            if (null != ctx) {
-                return DelegatingSecurityContextRunnable.create(code, ctx);
-            } else {
-                return code;
-            }
-        }
-    }
-    
-    private static final Executor RUN_IN_PLACE = Runnable::run;
-    private static final BiConsumer<DelegatingSecurityContextExecutor, SecurityContextHolderStrategy> SET_SECURITY_CONTEXT_HOLDER_STRATEGY = 
-            createOptionalSetter(DelegatingSecurityContextExecutor.class, "setSecurityContextHolderStrategy", SecurityContextHolderStrategy.class);
-    
-    private static <T, V> BiConsumer<T, V> createOptionalSetter(Class<T> beanClass, String setterName, Class<V> propertyType) {
-        
-        MethodHandles.Lookup lookup = MethodHandles.lookup();
-        MethodType setterType = MethodType.methodType(void.class, propertyType);
-        MethodType samMethodType = MethodType.methodType(void.class, Object.class, Object.class);
-        
-        try {
-            MethodHandle setterHandle = lookup.findVirtual(beanClass, setterName, setterType);
-            MethodType implMethodType = setterHandle.type();
-
-            CallSite site = LambdaMetafactory.metafactory(
-                lookup,                                     // Caller lookup object
-                "accept",                                   // Name of the SAM (Single Abstract Method)
-                MethodType.methodType(BiConsumer.class),    // Invoked type
-                samMethodType,                              // SAM signature after type erasure
-                setterHandle,                               // Implementation method handle
-                implMethodType                              // Actual implementation signature
-            );
-
-            try {
-                BiConsumer<T, V> setter = (BiConsumer<T, V>) site.getTarget().invokeExact();
-                return setter;
-            } catch (Throwable ex) {
-                throw new RuntimeException(ex);
-            }
-        } catch (NoSuchMethodException ex) {
-            return null;
-        } catch (IllegalAccessException | LambdaConversionException ex) {
-            throw new RuntimeException(ex);
+            return code;
         }
     }
     
